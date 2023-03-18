4e705ca248d9cd4de499e448e4d17035c11dd1a1
MinusCK <minuscheung1008@gmail.com>
2023-03-19 06:43:43 +0800

    add timeline

--- src/component/aboutme.tsx
+++ src/component/aboutme.tsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Row, Image, Container } from "react-bootstrap";
 import nav_logo from "../assets/image/logo_24px.png";
 import about_me from "../assets/image/about-temp.png";
+import { CameraReels, Motherboard, Soundwave } from "react-bootstrap-icons";
 
 function AboutMe() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -48,6 +49,91 @@ function AboutMe() {
               that leave a lasting impression on viewers.
             </div>
             <p>If I am a</p>
+            {/* <Row>
+              <Col sm={12} md={12}> */}
+            <div className="wrapper">
+              <div className="center-line">
+                <a href="#" className="scroll-icon">
+                  <i className="fas fa-caret-up"></i>
+                </a>
+              </div>
+              <div className="row row-1">
+                <section>
+                  <i className="icon fas fa-home">
+                    <CameraReels className="timeline-icon-color" />
+                  </i>
+
+                  <div className="details">
+                    <span className="title">Become a wedding Videographer</span>
+                    <div style={{ flex: "1" }}></div>
+                    <span style={{ color: "black" }}>
+                      Jan
+                      <br />
+                      2015
+                    </span>
+                  </div>
+                  <p className="time-line-p">
+                    Lorem ipsum dolor sit ameters consectetur
+                  </p>
+                </section>
+              </div>
+              <div className="row row-2">
+                <section>
+                  <i className="icon fas fa-star">
+                    <Motherboard className="timeline-icon-color" />
+                  </i>
+                  <div className="details">
+                    <span className="title">
+                      Graduate from City University of Hong Kong
+                    </span>
+                    <span style={{ color: "black" }}>
+                      May
+                      <br />
+                      2018
+                    </span>
+                  </div>
+                  <p className="time-line-p">
+                    Lorem ipsum dolor sit ameters consectetur
+                  </p>
+                </section>
+              </div>
+              <div className="row row-1">
+                <section>
+                  <i className="icon fas fa-rocket">
+                    {" "}
+                    <Soundwave className="timeline-icon-color" />
+                  </i>
+                  <div className="details">
+                    <span className="title">Become a Media Specialist</span>
+                    <span style={{ color: "black", alignItems: "right" }}>
+                      Feb <br />
+                      2021
+                    </span>
+                  </div>
+                  <p className="time-line-p">
+                    Lorem ipsum dolor sit ameters consectetur
+                  </p>
+                </section>
+              </div>
+              <div className="row row-2">
+                <section>
+                  <i className="icon fas fa-globe"></i>
+                  <div className="details">
+                    <span className="title">Become a Junior Web Developer</span>
+                    <span style={{ color: "black", alignItems: "right" }}>
+                      Jun
+                      <br />
+                      2022
+                    </span>
+                  </div>
+                  <p className="time-line-p">
+                    Lorem ipsum dolor sit ameters consectetur
+                  </p>
+                </section>
+              </div>
+            </div>
+            {/* </Col>
+            </Row> */}
             <Row>
               <Col sm={12} md={12}>
                 <div
